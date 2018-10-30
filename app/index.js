f96799f6e17d14df33433849ff751daa56436d0d
Dimitri Buhon <dbuhon@LVL545.local>
2018-10-30 11:22:59 -0400

    poc

--- app/index.js
+++ app/index.js
@@ -4,9 +4,14 @@ import { Header } from './components/header';
 import { OrderForm } from './components/orderForm';
 import { OrderList } from './components/orderList';
 import { OrderProvider } from './contexts/orders.context';
+import './index.scss';
+
+const video = require('./assets/sushi-video.mp4');
 
 export const Application = () => (
   <div>
+  <video autoPlay loop src={video} type="video/mp4" />
+
     <Header />
     <OrderProvider>
       <OrderForm />
